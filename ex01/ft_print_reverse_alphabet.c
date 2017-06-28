07b98cf4ca3a2cbc36c7aafb9624a86280868f7d
Flavio Wuensche <fwuensch@student.42.us.org>
2017-06-28 16:44:47 -0700

    Remove unnecessary return

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: fwuensch <fwuensch@student.42.us.or>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/28 16:14:32 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/06/28 16:19:27 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/06/28 16:30:52 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 void	ft_putchar(i)
 {
 	write(1, &i, 1);
-	return;
 }
 
 void	ft_print_reverse_alphabet(void)
