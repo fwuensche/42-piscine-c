5387f98d91396966f0f00f611738b23934e36fbd
Flavio Wuensche <fwuensch@student.42.us.org>
2017-06-29 15:48:21 -0700

    Finish ex06

--- ex06/ft_putnbr.c
+++ ex06/ft_putnbr.c
@@ -6,16 +6,9 @@
 /*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/29 15:19:20 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/06/29 15:47:47 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/06/29 15:48:11 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
 
 void	ft_putnbr(int nb)
 {
@@ -33,18 +26,4 @@ void	ft_putnbr(int nb)
 	{
 		ft_putchar(nb + 48);
 	}
-}
-
-int		main(void)
-{
-	ft_putnbr(-123456);
-	ft_putchar(' ');
-	ft_putnbr(0);
-	ft_putchar(' ');
-	ft_putnbr(12);
-	ft_putchar(' ');
-	ft_putnbr(123);
-	ft_putchar(' ');
-	ft_putnbr(1234);
-	return (0);
 }
