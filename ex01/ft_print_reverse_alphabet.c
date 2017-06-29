e424399657b9cc2364ccc668dcca4f2d6af81a4f
Flavio Wuensche <fwuensch@student.42.us.org>
2017-06-28 20:19:46 -0700

    Normalize ex01

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -12,9 +12,9 @@
 
 #include <unistd.h>
 
-void	ft_putchar(i)
+void	ft_putchar(char c)
 {
-	write(1, &i, 1);
+	write(1, &c, 1);
 }
 
 void	ft_print_reverse_alphabet(void)
@@ -22,15 +22,15 @@ void	ft_print_reverse_alphabet(void)
 	char	i;
 
 	i = 'z';
-	while ( i >= 'a' )
+	while (i >= 'a')
 	{
 		ft_putchar(i);
 		i--;
 	}
 }
 
-int main()
+int		main(void)
 {
 	ft_print_reverse_alphabet();
+	return (0);
 }
-
