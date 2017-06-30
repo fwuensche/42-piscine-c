9d68a49d56a0583e0ea7d05bf73e61893e874954
Flavio Wuensche <fwuensch@student.42.us.org>
2017-06-29 21:54:09 -0700

    Start 04

---
+++ ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/29 21:42:38 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/06/29 21:53:43 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int div;
+	int mod;
+
+	div = *a / *b;
+	mod = *a % *b;
+	*a = div;
+	*b = mod;
+}
+
+int		main(void)
+{
+	int a;
+	int b;
+
+	a = 78;
+	b = 10;
+	ft_ultimate_div_mod(&a, &b);
+	printf("%d\n", a);
+	printf("%d\n", b);
+	return (0);
+}
