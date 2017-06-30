ba59bdb72863fc61fa2cef1db35c2c859c9b690d
Flavio Wuensche <fwuensch@student.42.us.org>
2017-06-29 22:04:54 -0700

    Start 05

---
+++ ex05/ft_putstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/29 21:54:37 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/06/29 22:04:39 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		ft_putchar(*str);
+		str = str + 1;
+	}
+}
+
+int		main(void)
+{
+	char name[] = "Flavio";
+
+	ft_putstr(name);
+	return (0);
+}
