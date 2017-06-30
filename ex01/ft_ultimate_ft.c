c77b06797cb5ad26ed76a6a27ff29c3486267ff4
Flavio Wuensche <fwuensch@student.42.us.org>
2017-06-29 21:19:31 -0700

    Improve 01

--- ex01/ft_ultimate_ft.c
+++ ex01/ft_ultimate_ft.c
@@ -1,28 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/06/29 19:53:41 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/06/29 21:19:20 by fwuensch         ###   ########.fr       */
+/*   Created: 2017/06/29 20:14:45 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/06/29 20:25:30 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ft(int *nbr)
+void	ft_ultimate_ft(int *********nbr)
 {
-	*nbr = 42;
+	*********nbr = 42;
 }
 
 int		main(void)
 {
 	int a;
-	int	*ptr;
+	int *ptr;
+	int **ptr2;
+	int ***ptr3;
+	int ****ptr4;
+	int *****ptr5;
+	int ******ptr6;
+	int *******ptr7;
 
 	a = 10;
-	ptr = &a;
-	ft_ft(ptr);
+	ptr = &
+	ft_ultimate_ft(&a);
 	printf("%d\n", a);
 	return (0);
 }
