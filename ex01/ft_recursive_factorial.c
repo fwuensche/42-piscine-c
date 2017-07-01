d882f3f76976f4a52b4c10427683970c95d2a743
Flavio Wuensche <fwuensch@student.42.us.org>
2017-06-30 23:46:36 -0700

    Implement 01

---
+++ ex01/ft_recursive_factorial.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/30 23:27:24 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/06/30 23:46:18 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	if (nb < 0 || nb >= 13)
+		return (0);
+	if (nb == 0)
+		return (1);
+	return (nb * ft_iterative_factorial(nb - 1));
+}
+
+int	main(void)
+{
+	printf("%d\n", ft_iterative_factorial(-1));
+	printf("%d\n", ft_iterative_factorial(0));
+	printf("%d\n", ft_iterative_factorial(3));
+	printf("%d\n", ft_iterative_factorial(13));
+	return (0);
+}
