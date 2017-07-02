ab38e5bca45eea3f5d35c9f1ebd3485a6b5695b9
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-01 23:33:48 -0700

    Treat one last case

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -6,7 +6,7 @@
 /*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/01 01:14:23 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/01 13:54:43 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/01 23:33:07 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@ int	ft_recursive_power(int nb, int power)
 		return (0);
 	if (power == 0 || nb == 1)
 		return (1);
+	if (power >= 31)
+		return (-1);
 	return (nb * ft_recursive_power(nb, power - 1));
 }
