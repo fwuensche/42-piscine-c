6dd7d597efcff0d1f499fd45b3a228008e8556cd
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-03 13:25:37 -0700

    Try to fix atoi

--- ex02/ft_atoi.c
+++ ex02/ft_atoi.c
@@ -6,24 +6,17 @@
 /*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/03 12:38:41 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/03 13:16:31 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/03 13:25:30 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_special_character(char *str)
-{
-	if (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\r'
-		|| *str == '\v' || *str == '\f')
-		return (1);
-	return (0);
-}
 
 int	ft_atoi(char *str)
 {
 	int	result;
 	int	sign;
 
-	while (*str != '\0' && ft_special_character(str))
+	while (*str == ' ' || *str == '\t' || *str == '\n'
+		|| *str == '\r' || *str == '\v' || *str == '\f')
 		str++;
 	sign = 1;
 	if (*str == '+')
