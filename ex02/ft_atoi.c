4b3f073041ec39b3c8289c9f4bbacd3b7634ed35
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-03 13:27:29 -0700

    Finally fix atoi (or not)

--- ex02/ft_atoi.c
+++ ex02/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/03 12:38:41 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/03 13:25:30 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/03 13:27:18 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	ft_atoi(char *str)
 	while (*str == ' ' || *str == '\t' || *str == '\n'
 		|| *str == '\r' || *str == '\v' || *str == '\f')
 		str++;
+	str--;
 	sign = 1;
 	if (*str == '+')
 		str++;
