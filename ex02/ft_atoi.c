2395d31d80d981f48b1a6017d3860ad5003301de
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-03 13:35:33 -0700

    Now atoi works!

--- ex02/ft_atoi.c
+++ ex02/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/03 12:38:41 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/03 13:27:18 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/03 13:35:21 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@ int	ft_atoi(char *str)
 	int	result;
 	int	sign;
 
+	result = 0;
+	sign = 1;
 	while (*str == ' ' || *str == '\t' || *str == '\n'
 		|| *str == '\r' || *str == '\v' || *str == '\f')
 		str++;
-	str--;
-	sign = 1;
 	if (*str == '+')
 		str++;
 	else if (*str == '-')
@@ -27,10 +27,7 @@ int	ft_atoi(char *str)
 		sign = -1;
 		str++;
 	}
-	else
-		str++;
-	result = 0;
-	while ((*str >= '0' && *str <= '9') && *str != '\0')
+	while (*str >= '0' && *str <= '9')
 	{
 		result = result * 10 + *str - '0';
 		str++;
