25acd9bccc8d5a6f7967e0d11cdbdb5380724c38
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-03 21:01:52 -0700

    Fix 10

--- ex10/ft_strcapitalize.c
+++ ex10/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: fwuensch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/03 20:37:02 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/03 20:56:51 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/03 21:01:03 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,33 @@ int		ft_is_alphanumeric(char c)
 	return (0);
 }
 
+int		ft_is_alpha(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
+
 char	*ft_strcapitalize(char *str)
 {
-	int i;
+	unsigned int i;
 
-	i = 0;
-	if (ft_is_alphanumeric(str[i]))
-		str[i] = str[i] + 'A' - 'a';
+	if ('a' <= str[0] && str[0] <= 'z')
+		str[0] = str[0] - 'a' + 'A';
+	i = 1;
 	while (str[i] != '\0')
 	{
-		if (ft_is_alphanumeric(str[i]) && !ft_is_alphanumeric(str[i - 1]))
+		if (ft_is_alpha(str[i]))
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] = str[i] + 'A' - 'a';
+			if (ft_is_alphanumeric(str[i - 1]))
+			{
+				if ('A' <= str[i] && str[i] <= 'Z')
+					str[i] = str[i] - 'A' + 'a';
+			}
+			else if ('a' <= str[i] && str[i] <= 'z')
+				str[i] = str[i] - 'a' + 'A';
 		}
 		i++;
 	}
+	return (str);
 }
