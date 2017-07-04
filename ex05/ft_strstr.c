7e35ecabc0c8de767672abd1e1942b8a2decd918
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-03 18:02:18 -0700

    New solution to 05

--- ex05/ft_strstr.c
+++ ex05/ft_strstr.c
@@ -6,29 +6,35 @@
 /*   By: fwuensch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/03 17:08:00 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/03 17:51:12 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/03 18:02:09 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char  *ft_strstr(char *str, char *to_find)
 {
-	char *result;
-	char *query;
+	int i;
+	int find_index;
+	int remember;
 
-	if (*to_find == '\0')
-		return (str);
-	while (*str != '\0')
+	find_index = 0;
+	i = 0;
+	if (to_find[0] == '\0')
+		return (&str[0]);
+	while (str[i] != '\0')
 	{
-		result = str;
-		query = to_find;
-		while (*str != '\0' && *query != '\0' && *str == *query)
+		if (str[i] == to_find[find_index])
 		{
-			str++;
-			query++;
+			remember = i;
+			while (str[i] == to_find[find_index])
+			{
+				if (to_find[find_index++ + 1] == '\0')
+					return (&str[remember]);
+				i++;
+			}
+			find_index = 0;
+			i = remember;
 		}
-		if (*(query - 1) == '\0')
-			return (result);
-		str = result + 1;
+		i++;
 	}
 	return (0);
 }
