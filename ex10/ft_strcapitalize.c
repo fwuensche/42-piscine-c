d2efb9543b5d4837e4678c29c845a6201a92c16c
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-03 20:57:02 -0700

    Fixing 10

--- ex10/ft_strcapitalize.c
+++ ex10/ft_strcapitalize.c
@@ -6,13 +6,13 @@
 /*   By: fwuensch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/03 20:37:02 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/03 20:53:32 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/03 20:56:51 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_is_alphanumeric(char c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <='Z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
 		|| (c >= '0' && c <= '9'))
 		return (1);
 	return (0);
@@ -22,6 +22,9 @@ char	*ft_strcapitalize(char *str)
 {
 	int i;
 
+	i = 0;
+	if (ft_is_alphanumeric(str[i]))
+		str[i] = str[i] + 'A' - 'a';
 	while (str[i] != '\0')
 	{
 		if (ft_is_alphanumeric(str[i]) && !ft_is_alphanumeric(str[i - 1]))
