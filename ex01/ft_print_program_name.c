cba72b01f4cf3ba6ce009086247fb36aa4504e1b
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-04 10:25:34 -0700

    Finish ex02

---
+++ ex01/ft_print_program_name.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/04 10:17:29 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/07/04 10:25:04 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	ft_putstr(argv[0]);
+	ft_putchar('\n');
+}
