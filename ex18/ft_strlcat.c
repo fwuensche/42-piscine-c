ec2667384ce5f21ce5a50075066dbec39d5718ef
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-03 22:49:56 -0700

    Fix 18

--- ex18/ft_strlcat.c
+++ ex18/ft_strlcat.c
@@ -6,26 +6,32 @@
 /*   By: fwuensch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/03 22:10:53 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/03 22:43:41 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/03 22:49:49 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	int i;
-	unsigned int j;
+	int j;
+	int src_size;
+	int dst_size;
+	int space_left;
 
 	i = 0;
-	j = 0;
-	while (dest[j])
-		j++;
-	while (src[i])
-	{
-		if (j < size - 1)
-			dest[j] = src[i];
-		j++;
+	dst_size = 0;
+	space_left = size;
+	while (src[i] != '\0')
 		i++;
-	}
-	dest[j - 1] = '\0';
-	return (j);
+	src_size = i;
+	while (dest[dst_size] != '\0' && space_left-- != 0)
+		dst_size++;
+	space_left = size - dst_size;
+	if (space_left == 0)
+		return (size + src_size);
+	i = 0;
+	j = dst_size;
+	while (src[i] != 0 && space_left-- > 1)
+		dest[j++] = src[i++];
+	return (src_size + dst_size);
 }
