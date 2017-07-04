cd251edd79c5bff599eda4a4b27458ab468d652a
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-03 21:25:06 -0700

    Fix 05

--- ex05/ft_strstr.c
+++ ex05/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/03 17:08:00 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/03 18:18:50 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/03 21:24:57 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,5 @@ char	*ft_strstr(char *str, char *to_find)
 		}
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
