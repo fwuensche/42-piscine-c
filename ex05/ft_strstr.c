0dd9cc1bdeaa36d6ad7525c5b4e4ffd21eabf4b9
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-03 18:10:07 -0700

    Normalize 05

--- ex05/ft_strstr.c
+++ ex05/ft_strstr.c
@@ -3,35 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fwuensch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/03 17:08:00 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/03 18:02:09 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/03 18:08:06 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char  *ft_strstr(char *str, char *to_find)
+char	*ft_strstr(char *str, char *to_find)
 {
 	int i;
 	int find_index;
 	int remember;
 
-	find_index = 0;
 	i = 0;
 	if (to_find[0] == '\0')
 		return (&str[0]);
 	while (str[i] != '\0')
 	{
+		find_index = 0;
 		if (str[i] == to_find[find_index])
 		{
 			remember = i;
+			find_index = 0;
 			while (str[i] == to_find[find_index])
 			{
 				if (to_find[find_index++ + 1] == '\0')
 					return (&str[remember]);
 				i++;
 			}
-			find_index = 0;
 			i = remember;
 		}
 		i++;
