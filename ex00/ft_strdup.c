37efd6090de08c7827b01ac8e4cea9b9a9c72760
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-05 20:15:53 -0700

    Finish 00

---
+++ ex00/ft_strdup.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/05 12:19:19 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/07/05 20:15:08 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	j = 0;
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[j] = src[i];
+		j++;
+		i++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
+
+int		ft_strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
+char	*ft_strdup(char *src)
+{
+	char *duplicate;
+
+	duplicate = (char*)malloc(sizeof(*duplicate) * (ft_strlen(src) + 1));
+	return (ft_strcpy(duplicate, src));
+}
