b564137266d3aa27f9ef74f9a41ade37213b4bca
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-06 14:38:43 -0700

    Almost there with 04

--- ex04/ft_split_whitespaces.c
+++ ex04/ft_split_whitespaces.c
@@ -3,21 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split_whitespaces.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fwuensch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/06 12:58:26 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/06 13:40:22 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/06 14:37:40 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
+
+#define WHITESPACE(x) x == '\t' || x == '\n' || x == ' '
+
+int				ft_word_count(char *str)
+{
+	int word_count;
+	int i;
+
+	i = 0;
+	word_count = 0;
+	while (str[i] != '\0')
+	{
+		while (WHITESPACE(str[i]) && str[i] != '\0')
+			i++;
+		while (!(WHITESPACE(str[i])) && str[i] != '\0')
+			i++;
+		while (WHITESPACE(str[i]) && str[i] != '\0')
+			i++;
+		word_count++;
+	}
+	return (word_count);
+}
+
+int				ft_word_size(char *str)
+{
+	int i;
+
+	i = 0;
+	while (WHITESPACE(str[i]) && str[i] != '\0')
+		str++;
+	while (!(WHITESPACE(str[i])) && str[i] != '\0')
+		i++;
+	return (i);
+}
+
 char	**ft_split_whitespaces(char *str)
 {
-	// copy the string to the i-th element of the array until it matches space/tab/linebreak
-	// increment i
-	// break while when end of string is found
-	// add a 0 to the last element of the array
-	// return arr[i][j]
-
 	int str_it;
 	int i;
 	int j;
@@ -25,25 +56,47 @@ char	**ft_split_whitespaces(char *str)
 	j = 0;
 
 	char **arr;
+	arr = (char**)malloc(sizeof(char*) * ft_word_count(str));
+	arr[0] = (char*)malloc(sizeof(char) * 6);
+	arr[1] = (char*)malloc(sizeof(char) * 6);
+	arr[2] = (char*)malloc(sizeof(char) * 6);
+	arr[3] = (char*)malloc(sizeof(char) * 1);
+	str_it = 0;
 	while (str[str_it] != '\0')
 	{
 		if (str[str_it] == ' ' || str[str_it] == '\n' || str[str_it] == '\t')
 		{
 			arr[i][j] = '\0';
 			if (j != 0)
+			{
 				i++;
-			str_it++;
+			}
 			j = 0;
 		}
 		else
 		{
 			arr[i][j] = str[str_it];
-			str_it++;
 			j++;
 		}
 		str_it++;
 	}
-	arr[i][j] = '0';
-	arr[i][j + 1] = '\0';
+	i++;
+	arr[i][0] = '\0';
 	return (arr);
+}
+
+int main()
+{
+	char name[] = "Caioza Flavio Gaucho";
+	char **ret;
+	int i;
+
+	ret = ft_split_whitespaces(name);
+	i = 0;
+	while (i < 4)
+	{
+		printf("%s\n", ret[i]);
+		i++;
+	}
+	return (0);
 }
