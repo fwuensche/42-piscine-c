e782ac5c246d11feebc3d7b83b4688edd27523ae
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-05 22:56:42 -0700

    Fix 03

--- ex03/ft_concat_params.c
+++ ex03/ft_concat_params.c
@@ -6,7 +6,7 @@
 /*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/05 20:54:51 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/05 22:17:54 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/05 22:56:33 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,45 +25,38 @@ int		ft_strlen(char *str)
 	return (len);
 }
 
-char	*ft_strcat(char *dest, char *src)
-{
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
 char	*ft_concat_params(int argc, char **argv)
 {
 	char *args;
 	int i;
+	int j;
+	int k;
 	int mem;
 
-	i = 1;
 	mem = 0;
+	i = 1;
 	while (i < argc)
 	{
 		mem += ft_strlen(argv[i]) + 1;
 		i++;
 	}
-	mem--;
 	args = (char*)malloc(sizeof(char) * mem);
 	i = 1;
-	while (i < argc - 1)
+	k = 0;
+	while (i < argc)
 	{
-		ft_strcat(argv[1], argv[i + 1]);
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			args[k] = argv[i][j];
+			j++;
+			k++;
+		}
 		i++;
+		if (i == argc)
+			args[k] = '\0';
+		else
+			args[k] = '\n';
 	}
 	return (args);
 }
