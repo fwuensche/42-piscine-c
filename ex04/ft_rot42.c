2597b5d7d6b5f62613bfd7b30df44a5a35732f4a
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-06 22:09:53 -0700

    Remove main from 04

--- ex04/ft_rot42.c
+++ ex04/ft_rot42.c
@@ -6,11 +6,9 @@
 /*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/06 21:45:21 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/06 22:08:03 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/06 22:09:39 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 char	*ft_rot42(char *str)
 {
@@ -26,11 +24,4 @@ char	*ft_rot42(char *str)
 		str++;
 	}
 	return ret;
-}
-
-int main(void)
-{
-	char name[] = "Flavio Wuensche 171";
-	printf("%s", ft_rot42(name));
-	return 0;
 }
