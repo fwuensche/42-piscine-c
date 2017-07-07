eca14299c11568bfb5b6c8efef8fa254b45511e3
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-06 19:04:41 -0700

    Improve 04

--- ex04/ft_split_whitespaces.c
+++ ex04/ft_split_whitespaces.c
@@ -6,14 +6,20 @@
 /*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/06 12:58:26 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/06 18:47:57 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/06 19:04:28 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-#define WHITESPACE(x) x == '\t' || x == '\n' || x == ' '
+int		ft_is_whitespace(char c)
+{
+	if (c == '\t' || c == '\n' || c == ' ')
+		return (1);
+	else
+		return (0);
+}
 
 int		ft_word_count(char *str)
 {
@@ -24,51 +30,43 @@ int		ft_word_count(char *str)
 	word_count = 0;
 	while (str[i])
 	{
-		while (WHITESPACE(str[i]) && str[i] != '\0')
+		while (ft_is_whitespace(str[i]) && str[i] != '\0')
 			i++;
-		while (!(WHITESPACE(str[i])) && str[i] != '\0')
+		while (!(ft_is_whitespace(str[i])) && str[i] != '\0')
 			i++;
-		while (WHITESPACE(str[i]) && str[i] != '\0')
+		while (ft_is_whitespace(str[i]) && str[i] != '\0')
 			i++;
 		word_count++;
 	}
 	return (word_count);
 }
 
-char	**ft_allocate_memory(char *str, int word_count)
+char	**ft_allocate_memory(char *str, int word_count, int i, int j)
 {
 	char	**arr;
-	int		i;
-	int		j;
-	int		str_it;
 
 	arr = (char**)malloc(sizeof(char*) * (word_count + 1));
-	str_it = 0;
-	i = 0;
-	j = 0;
-	while (WHITESPACE(str[str_it]))
-		str_it++;
-	while (str[str_it] != '\0')
+	while (ft_is_whitespace(*str))
+		str++;
+	while (*str)
 	{
-		if (WHITESPACE(str[str_it]))
+		if (ft_is_whitespace(*str))
 		{
-			arr[i] = (char*)malloc(sizeof(char) * (j + 1));
-			while (WHITESPACE(str[str_it]))
-				str_it++;
-			str_it--;
-			i++;
+			arr[i++] = (char*)malloc(sizeof(char) * (j + 1));
+			while (ft_is_whitespace(*str))
+				str++;
+			str--;
 			j = 0;
 		}
 		else
 			j++;
-		if (str[str_it + 1] == '\0')
+		if (*(str++ + 1) == '\0')
 			arr[i] = (char*)malloc(sizeof(char) * (j + 1));
-		str_it++;
 	}
-	if (!(WHITESPACE(str[str_it - 1])))
+	if (!(ft_is_whitespace(*(str - 1))))
 		i++;
 	arr[i] = 0;
-	return arr;
+	return (arr);
 }
 
 char	**ft_split_whitespaces(char *str)
@@ -83,14 +81,14 @@ char	**ft_split_whitespaces(char *str)
 	j = 0;
 	str_it = 0;
 	word_count = ft_word_count(str);
-	arr = ft_allocate_memory(str, word_count);
-	while (WHITESPACE(str[str_it]))
+	arr = ft_allocate_memory(str, word_count, 0, 0);
+	while (ft_is_whitespace(str[str_it]))
 		str_it++;
 	while (i < word_count)
 	{
 		if (str[str_it] == ' ' || str[str_it] == '\n' || str[str_it] == '\t')
 		{
-			while (WHITESPACE(str[str_it]))
+			while (ft_is_whitespace(str[str_it]))
 				str_it++;
 			str_it--;
 			arr[i][j] = '\0';
@@ -107,9 +105,9 @@ char	**ft_split_whitespaces(char *str)
 
 int main()
 {
-	char name[] = "  \t\n Caio Flavio Écrire\n\t \nCasa Hello\tWorld\t\n   \t ";
-	char **ret;
-	int i;
+	char	name[] = "  \t\n Caio Flavio Écrire\n\t \nCasa Hello\tWorld\t\n   \t ";
+	char	**ret;
+	int		i;
 
 	ret = ft_split_whitespaces(name);
 	i = 0;
