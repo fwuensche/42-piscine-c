12386a309521faadd8715c78fa5bb6b62a57e556
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-09 22:25:41 -0700

    Almost there

--- ex00/main.c
+++ ex00/main.c
@@ -6,48 +6,126 @@
 /*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/09 16:16:12 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/09 19:23:01 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/09 22:21:40 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-// void	solve_sudoku(int **tab)
-// {
-// 	// run through the table
-// 		// if value == 0
-// 			// get possible values
-// 			// if no possible value, return 0
-// 			// for each possible value
-// 				// fill the current value in the table
-// 				// return function pointing to the next place
-// 	while (i < 10)
-// 		while (j < 10)
-// 			if (tab[i][j] == 0)
-// 				get_possible_values(i, j);
-// }
+/*
+ * Checks to see if a particular value is presently valid in a
+ * given position.
+ */
+int		is_valid(char number, char grid[9][9], int row, int column) {
+	int i = 0;
+	int sectorRow = 3 * (row / 3);
+	int sectorCol = 3 * (column / 3);
+	int row1 = (row + 2) % 3;
+	int row2 = (row + 4) % 3;
+	int col1 = (column + 2) % 3;
+	int col2 = (column + 4) % 3;
 
-void	**build_table(char **argv)
+	/* Check for the value in the given row and column */
+	while (i < 9)
+	{
+		if (grid[i][column] == number)
+			return (0);
+		if (grid[row][i] == number)
+			return (0);
+		i++;
+	}
+
+	/* Check the remaining four spaces in this sector */
+	if (grid[row1 + sectorRow][col1 + sectorCol] == number)
+		return 0;
+	if (grid[row2 + sectorRow][col1 + sectorCol] == number)
+		return 0;
+	if (grid[row1 + sectorRow][col2 + sectorCol] == number)
+		return 0;
+	if (grid[row2 + sectorRow][col2 + sectorCol] == number)
+		return 0;
+	return 1;
+}
+
+int		solve_sudoku(char grid[9][9], int i, int j)
+{
+	char	n;
+
+	if (i == 9)
+		return (1);
+
+	if (grid[i][j] != '0') {
+		if (j == 8) {
+			if (solve_sudoku(grid, i+1, 0)) return 1;
+		} else {
+			if (solve_sudoku(grid, i, j+1)) return 1;
+		}
+		return (0);
+	}
+
+	n = '1';
+	while (n <= '9') {
+		if(is_valid(n, grid, i, j)) {
+			grid[i][j] = n;
+			if (j == 8) {
+				if (solve_sudoku(grid, i + 1, 0))
+					return (1);
+			} else {
+				if (solve_sudoku(grid, i, j + 1))
+					return (1);
+			}
+			grid[i][j] = 0;
+		}
+		n++;
+	}
+
+
+	if (grid[i][j] == '0')
+	{
+		n = '1';
+		while (n <= '9')
+		{
+			if (is_valid(n, grid, i, j))
+			{
+				grid[i][j] = n;
+				if (j == 8)
+				{
+					if (solve_sudoku(grid, i + 1, 0))
+						return (1);
+				}
+				else
+				{
+					if (solve_sudoku(grid, i, j + 1))
+						return (1);
+				}
+				grid[i][j] = 0;
+			}
+			n++;
+		}
+	}
+	return (0);
+}
+
+void		build_table(char **argv, char grid[9][9])
 {
 	int i;
 	int j;
 
-	i = 1;
-	while (i < 10)
+	i = 0;
+	while (i < 9)
 	{
 		j = 0;
 		while (j < 9)
 		{
 			if (argv[i][j] == '.')
-			{
-				argv[i][j] = '0';
-			}
+				grid[i][j] = '0';
+			else
+				grid[i][j] = argv[i][j];
 			j++;
 		}
 		i++;
 	}
-	return (0);
 }
 
 int		ft_strlen(char *str)
@@ -99,22 +177,22 @@ int		input_is_valid(int argc, char **argv)
 	return (1);
 }
 
-void	print_table(char **tab)
+void	print_table(char grid[9][9])
 {
 	int		i;
 	int		j;
 
-	i = 1;
-	while (i < 10)
+	i = 0;
+	while (i < 9)
 	{
 		j = 0;
 		while (j < 8)
 		{
-			write(1, &tab[i][j], 1);
+			write(1, &grid[i][j], 1);
 			write(1, " ", 1);
 			j++;
 		}
-		write(1, &tab[i][j], 1);
+		write(1, &grid[i][j], 1);
 		write(1, "\n", 1);
 		i++;
 	}
@@ -122,17 +200,19 @@ void	print_table(char **tab)
 
 int		main(int argc, char **argv)
 {
+	char	grid[9][9];
+
 	if (!(input_is_valid(argc, argv)))
 	{
 		write(1, "Error\n", 6);
 		return (0);
 	}
 
-	build_table(argv);
-	// solution = solve_sudoku(**tab);
+	build_table(argv + 1, grid);
+	solve_sudoku(grid, 0, 0);
 
 	// if (solution)
-	print_table(argv);
+	print_table(grid);
 	// else
 	// {
 	// 	write(1, "Error\n", 6);
