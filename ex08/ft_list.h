6f30df682d462521fe1e9b17012c6e06908b6632
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-12 20:09:04 -0700

    Finish 08

---
+++ ex08/ft_list.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/12 19:46:35 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/07/12 19:46:54 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct		s_list
+{
+	struct s_list	*next;
+	void			*data;
+}					t_list;
+
+t_list				*ft_create_elem(void *data);
+void				ft_list_reverse(t_list **begin_list);
+
+#endif
---
+++ ex08/ft_list_reverse.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_reverse.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/12 19:46:14 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/07/12 20:08:56 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_reverse(t_list **begin_list)
+{
+	t_list	*prev;
+	t_list	*next;
+	t_list	*curr;
+
+	prev = 0;
+	next = 0;
+	curr = *begin_list;
+	while (curr)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	*begin_list = prev;
+}
