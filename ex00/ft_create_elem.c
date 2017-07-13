f9c68632a6ad6eee34f7b7c6353bb2f4a2639e62
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-12 17:07:39 -0700

    Finish 00

---
+++ ex00/ft_create_elem.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_elem.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/12 16:28:08 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/07/12 17:06:30 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_list.h"
+
+t_list		*ft_create_elem(void *data)
+{
+	t_list *list;
+
+	list = malloc(sizeof(t_list));
+	if (list)
+	{
+		list->data = data;
+		list->next = NULL;
+	}
+	return (list);
+}
