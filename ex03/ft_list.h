421cc9c256cfa6de70ef0110a3cb25ef68a03aa6
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-12 17:35:57 -0700

    Headers 03

---
+++ ex03/ft_list.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/12 16:18:28 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/07/12 17:35:45 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct		s_list
+{
+	struct s_list	*next;
+	void			*data;
+}					t_list;
+
+t_list				*ft_create_elem(void *data);
+int					ft_list_size(t_list *begin_list);
+
+#endif
