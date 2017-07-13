623115fb1600c3249581dcbec560c56db9bc9042
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-12 19:10:14 -0700

    Finish 05

---
+++ ex05/ft_list.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/12 17:44:56 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/07/12 17:45:08 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct		s_list
+{
+	struct s_list	*next;
+	void			*data;
+}					t_list;
+
+t_list				*ft_create_elem(void *data);
+t_list				*ft_list_push_params(int ac, char **av);
+
+#endif
