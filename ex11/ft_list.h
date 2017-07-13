ca9f4df67b66c3e7f2c548450afa13413c9047a5
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-12 20:30:39 -0700

    Finish 07

---
+++ ex11/ft_list.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/12 20:24:19 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/07/12 20:30:28 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct		s_list
+{
+	struct s_list	*next;
+	void			*data;
+}					t_list;
+
+t_list				*ft_list_find(t_list *begin_list, void *data_ref,
+									int (*cmp)());
+
+#endif
