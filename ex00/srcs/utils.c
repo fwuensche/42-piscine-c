4b19c8165bf0e2032126acb3a390ecabcdf254fa
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-15 23:46:59 -0700

    utils.c

--- ex00/srcs/utils.c
+++ ex00/srcs/utils.c
@@ -6,29 +6,34 @@
 /*   By: fwuensch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/15 20:19:19 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/15 21:54:00 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/15 23:46:32 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_rush.h"
 
+#include <stdio.h>
+
 int		is_valid_format(char *str)
 {
 	int rows;
 	int cols;
-	int j;
+	int col;
 
 	rows = count_rows(str);
 	cols = count_cols(str);
 	while (*str)
 	{
-		j = 0;
-		while (j < cols && *str)
+		col = 0;
+		while (col < cols && *str)
 		{
-			if (*str == '\n')
-				return (0);
 			str++;
-			j++;
+			col++;
+		}
+		if (*str != '\n')
+		{
+			write(1, "aucune\n", 7);
+			return (0);
 		}
 		str++;
 	}
