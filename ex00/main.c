b1d187e3f62d5956320b3421cf486c8f48a371ad
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-16 23:31:54 -0700

    Finish eval

---
+++ ex00/main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/16 22:25:21 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/07/16 22:38:28 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int		eval_expr(char *str)
+{
+	return (0);
+}
+
+int		main(int ac, char **av)
+{
+	if (ac > 1)
+	{
+		ft_putnbr(eval_expr(av[1]));
+		ft_putchar(’\n’);
+	}
+	return (0);
+}
