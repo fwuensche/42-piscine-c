8c9ba5c42889726eaa9bcd96777cd075036e93a1
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-18 15:52:11 -0700

    Solve seg fault

--- bsq.c
+++ bsq.c
@@ -6,7 +6,7 @@
 /*   By: pebraun <pebraun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/17 12:34:40 by pebraun           #+#    #+#             */
-/*   Updated: 2017/07/18 15:14:18 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/18 15:50:17 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,33 @@
  * and its length (never 0)
  */
 
+int		is_valid_square(int **map, int i, int j, int size)
+{
+	int obstacles;
+
+	obstacles = 0;
+	obstacles += map[i + size][j + size];
+	obstacles -= ft_val(map, i - 1, j + size);
+	obstacles -= ft_val(map, i + size, j - 1);
+	obstacles += ft_val(map, i - 1, j - 1);
+
+	if (obstacles > 0)
+		return (0);
+	else
+		return (1);
+}
+
 // need to return indices. make them int *'s?
 int		bsq(int **pt_sums, int width, int *ri, int *rj)
 {
 	int size;
 	int	i;
 	int j;
+	int lines;
+
+	lines = 0;
+	while (pt_sums[lines])
+		lines++;
 
 	size = 0;
 	i = 0;
@@ -31,12 +52,9 @@ int		bsq(int **pt_sums, int width, int *ri, int *rj)
 		j = 0;
 		while (j < width)
 		{
-			while ((pt_sums[i + size]) && (j + size < width) &&
-				((pt_sums[i + size][j + size] - ft_val(pt_sums, i - 1, j + size)
-					- ft_val(pt_sums, i + size, j - 1) + ft_val(pt_sums, i - 1, j - 1)) < 1))
+			while ((j + size < width) && (i + size < lines) && is_valid_square(pt_sums, i, j, size))
 			{
 				size++;
-				printf("size: %i\ti: %i\tj: %i\n", size, i, j);
 				*ri = i;
 				*rj = j;
 			}
--- ft.h
+++ ft.h
@@ -6,7 +6,7 @@
 /*   By: pebraun <pebraun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/17 13:19:40 by pebraun           #+#    #+#             */
-/*   Updated: 2017/07/18 15:01:05 by fwuensch         ###   ########.fr       */
+/*   Updated: 2017/07/18 15:50:15 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,7 @@ void	free_chararr(char **map);
 void	free_intarr(int **arr);
 char	**build_map(char *str);
 void	print_solution(char *str, int size, int i, int j);
+int		is_valid_square(int **map, int i, int j, int size);
 
 char	*read_file(int fd);
 char	*realloc_and_strcat(char *s1, char *s2);
